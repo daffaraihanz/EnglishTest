72fb0d4c077063c8493d2e05c5fe1ea70d90443b
Petrus <daffa@MacBook-Air-Rz.local>
2022-05-04 19:35:02 +0700

    add component modal on screen grammar,listening and reading final

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -10,6 +10,32 @@ const arrowQuestion = document.getElementById("arrowQuestion");
 
 let a = 1;
 
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
 function toggleArrowQuestionFunction(){
     if(a == 1){
         listQuestionBody.classList.add("lolo");
